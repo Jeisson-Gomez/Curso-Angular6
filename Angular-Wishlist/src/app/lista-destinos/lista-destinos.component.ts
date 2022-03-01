a5ef078314240624e8984147240853b39e6fa0d8
Jeisson Gomez <71471533+jeypitbull@users.noreply.github.com>
2022-03-01 15:37:21 -0500

    Video 1

---
+++ Angular-Wishlist/src/app/lista-destinos/lista-destinos.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit, Input} from '@angular/core';
+
+@Component({
+  selector: 'app-lista-destinos',
+  templateUrl: './lista-destinos.component.html',
+  styleUrls: ['./lista-destinos.component.css']
+})
+export class ListaDestinosComponent implements OnInit {
+  @Input() destinos: string[];
+  constructor() {
+    this.destinos = ['Barcelona', 'Buenos Aires', 'Lima', 'Bogota'];
+  }
+
+  ngOnInit(): void {
+  }
+
+}
